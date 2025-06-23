86697a087ccf8187699ca5a37c1ff2e9d5de6581
N-Dave-08 <cdave.dev@gmail.com>
2025-06-23 21:08:35 +0800

    Add modules and packages example script
    - Introduced `08_modules_and_packages.py` to demonstrate the use of modules and packages in Python.
    - Implemented functions for greeting and squaring a number using custom package `mypackage`.
    - Included examples of importing and utilizing standard and custom modules.

---
+++ 08_modules_and_packages.py
@@ -0,0 +1,15 @@
+import math
+import mypackage.mymodule
+import mypackage.square
+import random
+
+print(math.sqrt(16))
+mypackage.mymodule.greet("John")
+
+
+# 1
+print(random.randint(1, 20))
+
+
+# 2
+print("Squared Number:", mypackage.square.square(2))
